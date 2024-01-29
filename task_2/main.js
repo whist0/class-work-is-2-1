43cd776bbff3aaea48bfb77154d8fe7d067215ab
whist0 <aidiktop11@gmail.com>
2024-01-29 10:50:04 +0300

    Написали решение задачи на конструкции if else

--- task_2/main.js
+++ task_2/main.js
@@ -1,7 +1,13 @@
-const age = 16;
-
-if (age >= 18) {
-    console.log("Вы совершеннолетний!");
+let statusPar = true
+console.log("Сегодня важные пары?")
+if ( statusPar === true) {
+    console.log("Сколько сегодня пар?")
+    let countPar = 4;
+    if (countPar < 4) {
+        console.log("Сиди дома");
+    } else {
+        console.log("Беги на учебу")
+    }
 } else {
-    console.log("Вы несовершеннолетний!");
+    console.log("Сиди дома!")
 }
