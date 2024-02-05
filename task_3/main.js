5d2fde1665ae1adbbec1b817dcc99f0155ab0fd0
whist0 <aidiktop11@gmail.com>
2024-02-05 13:49:34 +0300

    Выполнили работу с объектами

--- task_3/main.js
+++ task_3/main.js
@@ -1,24 +1,25 @@
-// let students  = ["Андрей", "Иван", "Стас"];
-// students[50] = "Индекс 50";
-// console.log(students);
+// let person = {
+//     name: "Tom",
+//     age: 18,
+//     hobbies: ["Кушать", "Спать", "Смотреть кино", "Читать книги"],
+//     contacts: {
+//         phone: [839424023, 832942394],
+//         email: ["tom@gmail.com", "tomerwg@gmail.com"],
+//         social: "www.gitbub.com"
+//     }
+// }
 
-// let college = [ ["Андрей", "Иван", "Стас"], [205, 111, 304] ];
-// college[1][1] = 112;
-// let kab = college[1][1];
-// console.log(kab);
+// console.log(`Имя - ${person["name"]}. Возраст - ${person["age"]}`)
+// console.log(person["hobbies"][2])
+// console.log(person["contacts"]["email"][1])
 
-let number = [1, 2, 3, 4, 5, 6, 7];
+let college = [
+    {name: "Tom", age: 23, group: "ИС-2-2"},
+    {name: "Bob", age: 15, group: "ИС-2-1"},
+    {name: "Stan", age: 23, group: "ИС-2-2"},
+    {name: "Harry", age: 17, group: "ИС-4-1"},
+];
 
-const coutLength = 5;
+college[0]["age"] = 24;
 
-if (number.length === coutLength) {
-    console.log("ok");
-} 
-else if (number.length < coutLength) {
-    let result = coutLength - number.length;
-    console.log(`Длина вашего массива: ${number.length}. Длину массива нужно увеличить до ${coutLength}. Прибавьте к массиву значение ${result}`);
-}
-else if (number.length > coutLength) {
-    result = number.length - coutLength;
-    console.log(`Длина вашего массива: ${number.length}. Длину массива нужно уменьшить до ${coutLength}. Удалите из массива ${result} значения`);
-}
+console.log(college[2]["name"]);
