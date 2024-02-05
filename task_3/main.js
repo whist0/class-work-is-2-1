9d9d4cd032a1a9f05f49bf0e319e7286434f182c
whist0 <aidiktop11@gmail.com>
2024-02-05 14:20:09 +0300

    Выполнили задачу с использованием объектов

--- task_3/main.js
+++ task_3/main.js
@@ -14,19 +14,71 @@
 // console.log(person["contacts"]["email"][1])
 
 let college = [
-    {name: "Tom", age: 23, group: "ИС-2-2"},
-    {name: "Bob", age: 15, group: "ИС-2-1"},
-    {name: "Stan", age: 23, group: "ИС-2-2"},
-    {name: "Harry", age: 17, group: "ИС-4-1"},
-];
+    {
+        name: "Harry",
+    age: 18,
+    group: "ИС-2-1",
+    studID: 321221,
+    typeLearning: "Очная",
+    task: ["Физ-ра", "МДК-02-02", "МДК-04-01"],
+    cash: 5500,
+    isStudent: true
+    },
 
-let count = 2
-let studentAge = college[0]["age"];
-let studentName = college[0]["name"];
+    {
+        name: "Yana",
+    age: 18,
+    group: "СА-2-1",
+    studID: 323421,
+    typeLearning: "Заочная",
+    task: ["ОБЖ", "Уголовное право", "Информатика"],
+    cash: 0,
+    isStudent: true
+    },
 
-if (studentAge >= 18) {
-    console.log(`Студент ${studentName} совершеннолетний. Ему ${studentAge} лет`)
-} 
-else if (studentAge < 18) {
-    console.log(`Студент ${studentName} не совершеннолетний. Ему ${studentAge} лет`)
+    {
+        name: "Tatyana",
+    age: 38,
+    isKurator: true,
+    kuratorGroup: "ИС-2-1",
+    kafedra: "Инф.технологии",
+    predmet: ["Мат. моделирование", "ЭВМ", "Математика"],
+    isStudent: false,
+    },
+
+    {
+        name: "Oksana",
+    age: 40,
+    isKurator: true,
+    kuratorGroup: "ИС-3-1",
+    kafedra: "Инф.технологии",
+    predmet: ["Мат. моделирование", "ЭВМ", "Математика"],
+    isStudent: false,
+    },
+]
+
+if (college[0]["isStudent"] === true) {
+    console.log(`Студент ${college[0]["name"]}`)
+    if (college[0]["age"] >= 18) {
+        console.log(`Студент ${college[0]["name"]} совершеннолетний.`)
+    }
+    else {
+        console.log(`Студент ${college[0]["name"]} не совершеннолетний.`)
+    }
+    if (college[0]["cash"] >= 4500) {
+        console.log(`Студент ${college[0]["name"]} получает повышенную стипендию.`)
+    }
+    else if (college[0]["cash"] <= 4500 && college[0]["cash"] >= 1) {
+        console.log(`Студент ${college[0]["name"]} получает обычную стипендию.`)
+    }
+    else if  ( college[0]["cash"] == 0 ) {
+        console.log(`Студент ${college[0]["name"]} не получает стипендию.`)
+
+}
+
+else if (college[0]["isStudent"] === false) {
+    console.log(`Вы выбрали студента ${college[0]["name"]}`)
+    if (college[0]["isKurator"] === true) {
+        console.log(`Преподаватель ${college[0]["name"]} куратор группы ${college[0]["name"]}`)
+    }
 }
